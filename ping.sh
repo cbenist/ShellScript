632becf63dbf9966b22a03ec2b75910c3ecb2e64
cbenist <cbenist@yahoo.com>
2015-12-04 14:55:33 +0000

    added

---
+++ ping.sh
@@ -0,0 +1,17 @@
+echo "Enter the url you would liketo check!"
+read website
+
+echo "Pinging: $website"
+
+
+count=$(ping -c5 $website | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+    
+    if [$count -eq 0];
+    then
+    echo "$website is down (ping failed)."
+    
+    else
+    
+    echo "$website is up and running."
+    
+    fi
